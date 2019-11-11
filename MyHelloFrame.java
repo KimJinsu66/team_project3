6c284b8cacd3e1f287828535872b599215c874cd
YooJehun <dbwpgns0525@gmail.com>
2019-11-11 13:17:30 +0900

    MyHelloFrame Class 주석 및 MyHelloListener 생성

--- MyHelloFrame.java
+++ MyHelloFrame.java
@@ -1,21 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
 /**
- * 여기에 MyHelloFrame 클래스 설명을 작성하십시오.
+ * Frame 역할을 하는 MyFrame 클래스
  * 
  * @author (2018315005 유제훈, 201671038 김진수) 
  * @version (2019.11.11)
  */
 public class MyHelloFrame extends JFrame
 {
-    public JPanel p;
+    
     public MyHelloFrame(){
         this.setTitle("실습_3(20191111)");
         this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        p = new JPanel();
-        this.add(lm);
+        MyHelloListener ml = new MyHelloListener();
+        this.add(ml);
 
         this.setVisible(true);
     }
