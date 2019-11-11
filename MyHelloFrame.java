c1cb36c5ea8bad30cae257c3f009e619c8e588be
JinsuKim <jinsu6688@gmail.com>
2019-11-11 13:26:22 +0900

    완성코드

--- MyHelloFrame.java
+++ MyHelloFrame.java
@@ -3,20 +3,20 @@ import java.awt.*;
 /**
  * Frame 역할을 하는 MyFrame 클래스
  * 
- * @author (2018315005 유제훈, 201671038 김진수) 
+ * @author (2018315005 유제훈, 2014671038 김진수) 
  * @version (2019.11.11)
  */
 public class MyHelloFrame extends JFrame
 {
-    
+
     public MyHelloFrame(){
         this.setTitle("실습_3(20191111)");
         this.setSize(300, 300);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        MyHelloListener ml = new MyHelloListener();
+        
+        MyHelloPanelListener ml = new MyHelloPanelListener();
         this.add(ml);
-
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 
--- MyHelloPanelListener.java
+++ MyHelloPanelListener.java
@@ -8,44 +8,44 @@ import java.awt.*;
  * @author (2014671038 김진수, 2018315005 유제훈 )
  * @version (2019.11.11)
  */
-public class MyHelloListener extends JPanel implements KeyListener,MouseListener
+public class MyHelloPanelListener extends JPanel implements KeyListener,MouseListener
 {
     private JLabel lm ;
     public final int FLYING_UNIT = 10;
     
-    public MyHelloListener(){
+    public MyHelloPanelListener(){
         lm = new JLabel("Hello");
         this.add(lm);
         this.addKeyListener(this);
         this.addMouseListener(this);
         this.setFocusable(true);
     }
-    
+
     public void mouseClicked(MouseEvent e){
         Component c = (Component)e.getSource();
         int x = e.getX();
         int y = e.getY();
         lm.setLocation(x,y);
-        
+
     }
     public void mouseEntered(MouseEvent e){}
     public void mouseExited(MouseEvent e){}
     public void mousePressed(MouseEvent e){}
     public void mouseReleased(MouseEvent e){}
-    
+
     public void keyPressed(KeyEvent e){
         int keyCode = e.getKeyCode();
-        
+
         switch(keyCode){
             case KeyEvent.VK_UP:
-                lm.setLocation(lm.getX(),lm.getY() -FLYING_UNIT); break;
+            lm.setLocation(lm.getX(),lm.getY() -FLYING_UNIT); break;
             case KeyEvent.VK_DOWN:
-                lm.setLocation(lm.getX(),lm.getY() +FLYING_UNIT); break;
+            lm.setLocation(lm.getX(),lm.getY() +FLYING_UNIT); break;
             case KeyEvent.VK_LEFT:
-                lm.setLocation(lm.getX() - FLYING_UNIT ,lm.getY()); break;
+            lm.setLocation(lm.getX() - FLYING_UNIT ,lm.getY()); break;
             case KeyEvent.VK_RIGHT:
-                lm.setLocation(lm.getX() +FLYING_UNIT ,lm.getY()); break;
-                
+            lm.setLocation(lm.getX() +FLYING_UNIT ,lm.getY()); break;
+
         }
     }
     public void keyReleased(KeyEvent e){}
